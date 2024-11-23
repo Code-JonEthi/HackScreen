84c4e31b9cf39918bb10f3fb0109ed709609b258
Code-JonEthi <180142204+Code-JonEthi@users.noreply.github.com>
2024-11-23 13:48:02 -0700

    Fixed memory leakage Spaced columns apart

--- hackscreen.c
+++ hackscreen.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
 #include <ncurses.h>
-#include <stdbool.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -14,9 +13,12 @@ struct column {
 struct column initialize_column(struct column col);
 struct column update_column(struct column col, int index);
 void print_column(struct column col, int x, WINDOW *screen);
+void free_columns(struct column columns[]);
 
 char key;
 int max_x, max_y;
+int width;
+int height;
 bool running;
 
 int main() {
@@ -30,12 +32,15 @@ int main() {
 
 
 	getmaxyx(stdscr, max_y, max_x);
+	width = ceil(max_x / 2);
+	height = max_y;
 	running = true;
+	int x;
 	
-	struct column columns[max_x];
+	struct column columns[width];
 
 	// Set every column with random values
-	for (int i = 0; i < max_x; i++) {
+	for (int i = 0; i < width; i++) {
 		columns[i] = initialize_column(columns[i]);
 	}
 
@@ -44,11 +49,13 @@ int main() {
 		// Set the color to green
 		init_pair(1, COLOR_GREEN, COLOR_BLACK);
 		attron(COLOR_PAIR(1));
-
-		for (int i = 0; i < max_x; i++) {
-			print_column(columns[i], i, stdscr);
+	
+		x = 1;
+		for (int i = 0; i < width; i++) {
+			print_column(columns[i], x, stdscr);
 			refresh();
-			columns[i] = update_column(columns[i], max_y - 1);
+			columns[i] = update_column(columns[i], height - 1);
+			x += 2;
 		}
 
 		key = getch();
@@ -57,6 +64,7 @@ int main() {
 		} else if (key == 'x') {
 			attroff(COLOR_PAIR(1));
 			endwin();
+			free_columns(columns);
 			running = false;
 			break;
 		}
@@ -65,17 +73,17 @@ int main() {
 }
 
 struct column initialize_column(struct column col) {
-	col.text = malloc(sizeof(char) * max_y);
+	col.text = malloc(sizeof(char) * height);
 	col.text[0] = rand() % 2 == 0 ? '0' : '1';
 	col.is_zero = rand() % 2 == 0;
 
-	for (int i = 1; i < max_y; i++) {
+	for (int i = 1; i < height; i++) {
 		if (col.text[i - 1] == '0') {
-			col.text[i] = rand() % 16 == 0 ? '1' : '0';
+			col.text[i] = rand() % 10 == 0 ? '1' : '0';
 		} else if (col.text[i - 1] == '1') {
-			col.text[i] = rand() % 16 == 0 ? col.is_zero ? '0' : ' ' : '1';
+			col.text[i] = rand() % 10 == 0 ? col.is_zero ? '0' : ' ' : '1';
 		} else {
-			col.text[i] = rand() % 16 == 0 ? '1' : ' ';
+			col.text[i] = rand() % 10 == 0 ? '1' : ' ';
 		}
 	}
 	return col;
@@ -87,12 +95,12 @@ struct column update_column(struct column col, int index) {
 		return update_column(col, index - 1);
 	} else {
 		if (col.text[index + 1] == '0') {
-			col.text[index] = rand() % 16 == 0 ? '1' : '0';
+			col.text[index] = rand() % 10 == 0 ? '1' : '0';
 			col.is_zero = !col.is_zero;
 		} else if (col.text[index + 1] == '1') {
-			col.text[index] = rand() % 16 == 0 ? col.is_zero ? '0' : ' ' : '1';
+			col.text[index] = rand() % 10 == 0 ? col.is_zero ? '0' : ' ' : '1';
 		} else {
-			col.text[index] = rand() % 16 == 0 ? '1' : ' ';
+			col.text[index] = rand() % 10 == 0 ? '1' : ' ';
 			col.is_zero = !col.is_zero;
 		}
 		return(col);
@@ -100,7 +108,14 @@ struct column update_column(struct column col, int index) {
 }
 
 void print_column(struct column col, int x, WINDOW *screen) {
-	for (int i = 0; i < max_y; i++) {
+	for (int i = 0; i < height; i++) {
 		mvwprintw(screen, i, x, "%c", col.text[i]);
 	}
 }
+
+void free_columns(struct column columns[]) {
+	for (int i = 0; i < width; i++) {
+		free(columns[i].text);
+	}
+}
+
